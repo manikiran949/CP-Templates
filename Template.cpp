959e9d09f75cd0239b80504560d15a7b1ab31be8
Batchu Mani Kiran <119949905+manikiran949@users.noreply.github.com>
2024-12-30 10:12:20 +0530

    Updated Template

--- Template.cpp
+++ Template.cpp
@@ -107,3 +107,18 @@ void solve(){
         
     }
 }
+
+/* things to check
+1.  int overflow or long long memory need
+2.  recursion/array/binary search/dp/loop bounds
+3.  precision
+4.  special cases(n=1,bounds)
+5.  delete debug statements
+6.  initialize(especially multi-tests)
+7.  = or == , n or m ,++ or -- , i or j , > or >= , < or <=
+8.  keep it simple and stupid
+9.  do not delete, use // instead
+10. operator priority
+11. is there anything extra to output?
+12. ...
+*/
